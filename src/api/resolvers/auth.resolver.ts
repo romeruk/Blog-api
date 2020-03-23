e0c91db071b1e03594467f6980e24b825d4b032d
andrii <romeruk21@gmail.com>
2020-03-23 16:53:13 +0200

    move register method from userservice to auth service

--- src/api/resolvers/auth.resolver.ts
+++ src/api/resolvers/auth.resolver.ts
@@ -3,19 +3,22 @@ import { Response } from 'express';
 import { ResGql, isAdminAccess } from '../../common/decorators/decorators';
 import { UseGuards } from '@nestjs/common';
 import { AuthService } from 'src/service/services/auth.service';
-import { UserLogInInput } from '../inputs/user/user.input';
+import { UserLogInInput, CreateUserInput } from '../inputs/user/user.input';
 import { UserType } from '../types/user/user.type';
 import { GqlAuthGuard } from 'src/common/guards/gql.guard';
 import { AdminGqlGuard } from 'src/common/guards/role.guard';
+import { UserService } from 'src/service/services/user.service';
 
 @Resolver('Auth')
 export class AuthResolver {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
-  @UseGuards(GqlAuthGuard)
-  @Query(returns => String)
-  hello() {
-    return '123';
+  @Mutation(returns => UserType)
+  async register(@Args('credentials') input: CreateUserInput) {
+    return await this.userService.createUser(input);
   }
 
   @Mutation(returns => UserType)
