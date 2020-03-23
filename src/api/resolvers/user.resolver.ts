b214b634149069b1c8b33dd37e8893e7c156e7c2
andrii <romeruk21@gmail.com>
2020-03-23 16:53:58 +0200

    move register method from userservice to auth service

--- src/api/resolvers/user.resolver.ts
+++ src/api/resolvers/user.resolver.ts
@@ -1,36 +1,64 @@
 import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
-import { UserType } from '../types/user/user.type';
+import { UserType, Users } from '../types/user/user.type';
 import { UserService } from 'src/service/services/user.service';
 import {
   CreateUserInput,
   ResetPasswordInput,
   UpdateUserInput,
 } from '../inputs/user/user.input';
-import { CurrentUser } from 'src/common/decorators/decorators';
+import { CurrentUser, isAdminAccess } from 'src/common/decorators/decorators';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/common/guards/gql.guard';
 import { IPayload } from 'src/common/interfaces/payload.interface';
+import { AdminGqlGuard } from 'src/common/guards/role.guard';
 
 @Resolver('User')
 export class UserResolver {
   constructor(private userService: UserService) {}
 
+  @UseGuards(GqlAuthGuard, AdminGqlGuard)
+  @isAdminAccess(true)
   @Mutation(returns => UserType)
-  async register(@Args('credentials') input: CreateUserInput) {
-    const user = await this.userService.createUser(input);
-    return user;
+  async createAdminUser(@Args('credentials') input: CreateUserInput) {
+    return await this.userService.createUser(input, true);
+  }
+
+  @UseGuards(GqlAuthGuard, AdminGqlGuard)
+  @isAdminAccess(true)
+  @Mutation(returns => UserType)
+  async removeUser(@Args('email') email: string) {
+    return await this.userService.removeUser(email);
+  }
+
+  @UseGuards(GqlAuthGuard, AdminGqlGuard)
+  @isAdminAccess(true)
+  @Mutation(returns => UserType)
+  async recoverUser(@Args('email') email: string) {
+    return await this.userService.recoverUser(email);
+  }
+
+  @UseGuards(GqlAuthGuard, AdminGqlGuard)
+  @isAdminAccess(true)
+  @Query(returns => UserType)
+  async findOneUser(@Args('email') email: string) {
+    return await this.userService.getUserByEmailAddress(email);
+  }
+
+  @UseGuards(GqlAuthGuard, AdminGqlGuard)
+  @isAdminAccess(true)
+  @Mutation(returns => Users)
+  async findAllUsers(@Args('limit') limit: number, @Args('page') page: number) {
+    return await this.userService.findAll(limit, page);
   }
 
   @Mutation(returns => UserType)
   async verifyUser(@Args('token') token: string) {
-    const user = await this.userService.verifyUserByToken(token);
-    return user;
+    return await this.userService.verifyUserByToken(token);
   }
 
   @Mutation(returns => UserType)
   async setPasswordResetToken(@Args('email') email: string) {
-    const user = await this.userService.setPasswordResetToken(email);
-    return user;
+    return await this.userService.setPasswordResetToken(email);
   }
 
   @Mutation(returns => UserType)
